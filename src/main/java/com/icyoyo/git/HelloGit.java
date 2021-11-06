1710d3c3a38e735578a68022ca3b9ddf67d04803
icyoyo <1533221279.com>
2021-11-06 18:43:13 +0800

    测试码云

--- src/main/java/com/icyoyo/git/HelloGit.java
+++ src/main/java/com/icyoyo/git/HelloGit.java
@@ -16,5 +16,6 @@ public class HelloGit {
         System.out.println("远程库更新");
         System.out.println("推送到码云");
         System.out.println("取消认证");
+        System.out.println("测试码云");
     }
 }
